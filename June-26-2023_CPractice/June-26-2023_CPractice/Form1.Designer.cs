e5b783445f52b2c51781ef49c29116fc6fe7db85
Brian Ortiz <77475041+BrianOrtizSE@users.noreply.github.com>
2023-06-25 13:14:12 -0500

    Add files via upload
    The button Moves when hovered on and more specifically max button movement is on screen size!

---
+++ June-26-2023_CPractice/June-26-2023_CPractice/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿
+namespace June_26_2023_CPractice
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClick = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnClick
+            // 
+            this.btnClick.AutoSize = true;
+            this.btnClick.Location = new System.Drawing.Point(225, 225);
+            this.btnClick.Name = "btnClick";
+            this.btnClick.Size = new System.Drawing.Size(137, 87);
+            this.btnClick.TabIndex = 0;
+            this.btnClick.Text = "Click";
+            this.btnClick.UseVisualStyleBackColor = true;
+            this.btnClick.MouseEnter += new System.EventHandler(this.btnClickEnter);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 561);
+            this.Controls.Add(this.btnClick);
+            this.Name = "frmMain";
+            this.Text = "Hello Brain";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.Resize += new System.EventHandler(this.frmMain_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClick;
+    }
+}
+
---
+++ June-26-2023_CPractice/June-26-2023_CPractice/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace June_26_2023_CPractice
+{
+    public partial class frmMain : Form
+    {
+        Random rnd = new Random();
+        int i = 0;
+
+        int intMaxWidth;
+        int intMaxHeight;
+
+        //int intMaxScreenX = frmMain.Size.Height
+        //int intMaxScreenY = 
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClickEnter(object sender, EventArgs e)
+        {
+            if (i < 5)
+            {
+                btnClick.Location = new Point(rnd.Next(0, intMaxWidth), rnd.Next(0, intMaxHeight));
+                i++;
+
+            }
+            else
+            {
+                btnClick.Location = new Point(rnd.Next(0, intMaxWidth), rnd.Next(0, intMaxHeight));
+                btnClick.Enabled = false;
+            }
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            intMaxHeight = this.Height - 100;
+            intMaxWidth = this.Width - 100;
+        }
+        private void frmMain_Resize(object sender, EventArgs e)
+        {
+            intMaxHeight = this.Height - 100;
+            intMaxWidth = this.Width - 100;
+        }
+    }
+}
